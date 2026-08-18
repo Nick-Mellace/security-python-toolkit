423db59d6a407e8f42509b42188a9e5d7bab4c61
Nick Mellace <nickmprodev@proton.me>
2026-08-18 07:33:22 -0400

    Expand DNS lookup to multiple record types.

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-from toolkit.dns_lookup import mx_lookup
+from toolkit.dns_lookup import dns_lookup
 
-query = input('Domain: ')
-mx_lookup(query)
+domain = input('Domain: ').casefold().strip()
+record_type = input('Record Type: ').upper().strip()
+
+supported_records = ['A', 'AAAA', 'MX', 'TXT']
+
+if record_type not in supported_records:
+    print('Unsupported record type.')
+else:
+    dns_lookup(domain, record_type)
--- toolkit/dns_lookup.py
+++ toolkit/dns_lookup.py
@@ -1,14 +1,17 @@
 import dns.resolver
 
-def mx_lookup(domain):
+def dns_lookup(domain, record_type):
     try: 
-        records = dns.resolver.resolve(domain, 'MX')    # Pull MX records and assign to variable
-        for record in records:
-            if str(record.exchange) == '.' and record.preference == 0:  # Handling for Null MX
-                print(f'{domain} publishes a Null MX record and does not accept mail.')
-            else:                          
+        records = dns.resolver.resolve(domain, record_type)   # Pull requested records and assign to variable
+        for record in records:                                  # Iterate through record values
+            if record_type == 'MX':
+                if str(record.exchange) == '.' and record.preference == 0:  # Handling for Null MX
+                    print(f'{domain} publishes a Null MX record and does not accept mail.')
+                else:                          
+                    print(record)
+            else:
                 print(record)                  
     except dns.resolver.NXDOMAIN:   
         print(f'{domain} does not exist.')
     except dns.resolver.NoAnswer:     
-        print(f'{domain} is a valid domain, but does not have any published MX records.')
+        print(f'{domain} is a valid domain, but does not have any published {record_type} records.')
